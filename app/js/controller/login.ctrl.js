ee9a01d0ccf65e696025243ea99e1b447ee22703
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-08-17 01:22:44 -0300

    Dataloading reposicionado.

--- app/js/controller/login.ctrl.js
+++ app/js/controller/login.ctrl.js
@@ -11,23 +11,27 @@
 		ctrl.dataLoading = false;
 
 		ctrl.login = function (administrador) {
+			ctrl.dataLoading = true;
 			administrador.senha = md5(administrador.senha);
 			adminService.login(administrador)
 				.then(function success(response) {
 					toastr.success("Bem vindo " + response.administrador.nome + "!");
 					$location.path('/');
 				}).catch(function error(error) {
+					ctrl.dataLoading = false;
 					toastr.error(error.data.causa || 'Erro no login! Verifique email e senha.');
 				});
 		};
 
 		ctrl.register = function (administrador) {
+			ctrl.dataLoading = true;
 			administrador.senha = md5(administrador.senha);
 			adminService.register(administrador)
 				.then(function success(response) {
 					toastr.success("Administrador cadastrado com sucesso!");
 					$location.path('/');
 				}).catch(function error(error) {
+					ctrl.dataLoading = false;
 					toastr.error(error.data.causa || 'Erro no cadastro! Tente novamente mais tarde.');
 				});
 		};
