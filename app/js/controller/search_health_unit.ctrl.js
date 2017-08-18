ead7e844efe8b3903c90ffcfcb6b6a6f012dcdfe
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-08-18 15:59:00 -0300

    Atualização na relação Unidade de Saúde e Especialidades.

--- app/js/controller/search_health_unit.ctrl.js
+++ app/js/controller/search_health_unit.ctrl.js
@@ -11,22 +11,8 @@
 			$http.get(endPointsService.buscarUnidadesDeSaude + neighborhood)
 				.then(function success(response) {
 					ctrl.units = response.data;
-					ctrl.units.forEach(function(data){
-						ctrl.searchHUEsp(data.id).then(function(esps){
-							data.especialidades = esps;
-						});
-					});
 				}).catch(function error(error) {
 					toastr.error(error.data.causa || error.data.message || "Erro na busca de unidades");
-				});
-		}
-
-		ctrl.searchHUEsp = function(id) {
-			return $http.get(endPointsService.api + "/especialidade/unidade/" + id)
-				.then(function successCallback(response) {
-					return response.data;
-				}).catch(function error(error) {
-					toastr.error(error.data.causa || error.data.message || "Sem comentários!");
 				});
 		}
 
