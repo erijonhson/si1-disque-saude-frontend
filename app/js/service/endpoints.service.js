d8cde6108058dc10edfb151af8022ff7effe1bf9
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-08-18 18:40:33 -0300

    Ajustando configurações para heroku

--- app/js/service/endpoints.service.js
+++ app/js/service/endpoints.service.js
@@ -9,9 +9,9 @@
 		ctrl.unauthorized = ['/administrador/'];
 
 		// remoto
-		// ctrl.api = 'https://disque-saude-backend.herokuapp.com/SpringBootRestApi/api';
+		ctrl.api = 'https://disque-saude-backend.herokuapp.com/SpringBootRestApi/api';
 		// local
-		ctrl.api = 'http://localhost:5000/SpringBootRestApi/api';
+		// ctrl.api = 'http://localhost:5000/SpringBootRestApi/api';
 
 		ctrl.adminLogin = ctrl.api + '/admin/login/';
 		ctrl.adminRegister = ctrl.api + '/admin/cadastrar/';
